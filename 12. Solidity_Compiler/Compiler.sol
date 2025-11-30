aa0e905c8cd2ed6fc22e756d0945d15eab67a5cb
M.Z <zeynalov@gmail.com>
2025-11-30 10:41:50 +0400

    Compiler section

---
+++ 12. Solidity_Compiler/Compiler.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.30;
+// SPDX-License-Identifier: UNLICENSED
+
+/*
+Compiler's output: 
+    - Bytecode -> which will then be deployed to Ethereum
+    - Application Binary Interface (ABI) -> a json file showing the methods the contract has...
+ABI seems to be like Open API. 
+
+Installation using npm
+npm install -g solc
+
+How to compile
+solcjs
+    --bin \ -> generate bytecode
+    --abi / -> generate ABI
+    --base-path . \ -> path where the contracts are located
+    Voter.sol -> contracts to compile
+
+*/
